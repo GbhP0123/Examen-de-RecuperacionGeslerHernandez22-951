576b21136c84999db1ac44ccaca61fe94d98d562
GbhP0123 <161140724+GbhP0123@users.noreply.github.com>
2024-06-22 10:32:43 -0600

    ejercicios del menu

--- src/Main.java
+++ src/Main.java
@@ -46,3 +46,44 @@ public class ExamenFinalVarianteA {
         scanner.close();
     }
 }
+
+public void calculadoraBasica() {
+    System.out.print("Ingrese el primer número: ");
+    double num1 = scanner.nextDouble();
+    System.out.print("Ingrese el segundo número: ");
+    double num2 = scanner.nextDouble();
+    System.out.print("Ingrese la operación (+, -, *, /): ");
+    char operacion = scanner.next().charAt(0);
+
+    double resultado = 0;
+    boolean operacionValida = true;
+
+    switch (operacion) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                System.out.println("Error: No se puede Dividir por Cero");
+                operacionValida = false;
+            }
+            break;
+        default:
+            System.out.println("Operación no válida.");
+            operacionValida = false;
+            break;
+    }
+
+    if (operacionValida) {
+        System.out.println("El resultado es: " + resultado);
+    }
+}
+
